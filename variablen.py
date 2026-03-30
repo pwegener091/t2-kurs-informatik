f5e13a51f64794db6dae42964c79e610674915eb
Patrick Wegener <pat-weg09@gmx.de>
2026-03-30 07:39:54 +0000

    Programme Woche 2 - Teil 2

---
+++ variablen.py
@@ -0,0 +1,25 @@
+name = "Anna" #Datentyp string
+a = 5 #Datentyp integer
+b = 2.5 #Datentyp float
+w = True #Datentyp boolean
+print(type(name))
+print(type(a))
+print(type(b))
+print(type(w))
+a = 6
+print(a)
+c = 7
+print(a == c)
+d = 6
+print(a == d)
+print(a != c)  #das Zeichen != bedeutet ungleich
+e = 12 / 2
+print(a == e)
+print(type(e))
+print(int(e))
+print(a)
+f = 2.6
+print(int(f))
+print(float(7))
+a = a+1
+print(a)
